7c00c9667d1685243d09ecbba78090907f337e3d
j0i0m0b0o <90911158+j0i0m0b0o@users.noreply.github.com>
2026-05-09 02:55:59 -0700

    Add files via upload

---
+++ src/OpenOracleErrors.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+abstract contract OpenOracleErrors {
+    error TokensCannotBeSame();
+    error NoReportToDispute();
+    error EthTransferFailed();
+    error InvalidAmount1();
+    error InvalidAmount2();
+    error InvalidStateHash();
+    error InvalidGasLimit();
+    error SettleTooEarly();
+    error AlreadySettled();
+    error ExactToken1CannotBeZero();
+    error SettleVsDisputeDelayTiming();
+    error MsgValueTooLow();
+    error MsgValueTooHigh();
+    error FeesTooHigh();
+    error MultiplierTooLow();
+    error ReportAlreadySubmitted();
+    error InvalidReportId();
+    error AddressCannotBeZero();
+    error InvalidAmount2Expected();
+    error InvalidTokenToSwap();
+    error NoReportYet();
+    error EscalationHalted();
+    error AmountsCannotBeZero();
+    error DisputeTooLate();
+    error DisputeTooEarly();
+    error NewPriceInsideFeeBoundary();
+    error ReportNotSettled();
+    error InvalidMode();
+    error InvalidTiming();
+}
