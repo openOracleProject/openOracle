5582c2be1905e91044e7825384b2dbeeb09ea0a7
j0i0m0b0o <90911158+j0i0m0b0o@users.noreply.github.com>
2026-08-24 14:33:02 -0700

    Add files via upload

---
+++ src/libraries/PuntErrors.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+library PuntErrors {
+    error TokensCannotBeSame();
+    error EthTransferFailed();
+    error InvalidAmount1();
+    error AddressCannotBeZero();
+    error ContractCannotBeParticipant();
+    error AmountsCannotBeZero();
+    error InvalidMsgValue();
+    error ZeroAmount();
+    error InvalidExpiration();
+    error InvalidExecutionLatency();
+    error InvalidLifecycleModule();
+    error InvalidFeeReceiver();
+    error InvalidSelector();
+    error InvalidLiquidationHeartbeat();
+    error LiquidationHeartbeatLive();
+    error ForcedTransaction();
+    error InvalidFulfillFee();
+    error InvalidSlippage();
+    error InvalidOracleParams();
+    error InvalidFulfillFeeParams();
+    error WrongHash();
+    error WrongOracleHash();
+    error NotActive();
+    error Expired();
+    error NotSwapper();
+    error NotMatched();
+    error CantBailOutYet();
+    error NothingToWithdraw();
+    error OracleSettlementNotEligible();
+    error MustBeZero();
+
+    error InvalidMaturity();
+    error InvalidMargin();
+    error InvalidFundingRate();
+    error InvalidReportId();
+    error NoOracleGame();
+    error OracleGameInProgress();
+    error CloseIntentLive();
+    error InvalidDutchParams();
+}
